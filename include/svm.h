d1a2349e5fea3aa6d481a900661f6af94c14c15e
Clany <clany.song@outlook.com>
2014-09-27 17:40:24 -0400

    Add license file

--- include/svm.h
+++ include/svm.h
@@ -1,18 +1,18 @@
 /////////////////////////////////////////////////////////////////////////////////
 // The MIT License(MIT)
-// 
+//
 // Copyright (c) 2014 by Tiangang Song
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files(the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions :
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
@@ -33,7 +33,6 @@
 
 _CLANY_BEGIN
 namespace ml {
-
 using SVMNode      = svm_node;
 using SVMProblem   = svm_problem;
 using SVMModel     = svm_model;
@@ -72,8 +71,8 @@ public:
 
     void train(const cv::Mat& train_data, cv::InputArray labels, const SVMParameter& params);
     void train(const cv::Mat& train_data, cv::InputArray labels,
-               int svm_type = C_SVC, int kernel_type = RBF, int k_fold = 10, 
-               cv::ParamGrid Cgrid = cv::SVM::get_default_grid(cv::SVM::C), 
+               int svm_type = C_SVC, int kernel_type = RBF, int k_fold = 10,
+               cv::ParamGrid Cgrid = cv::SVM::get_default_grid(cv::SVM::C),
                cv::ParamGrid gammaGrid = cv::SVM::get_default_grid(cv::SVM::GAMMA));
 
     double predict(cv::InputArray sample) const;
@@ -92,7 +91,6 @@ private:
     SVMParameter params;
     unique_ptr<SVMModel, SVMModelDeletor> model_ptr;
 };
-
 } // End namespace ml
 _CLANY_END
 
